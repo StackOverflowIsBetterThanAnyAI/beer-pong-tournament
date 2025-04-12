3cc9cc7460245672a012a706ad9ed1dcedf5f7b2
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-04-12 11:16:58 +0200

    store username after logging out

--- src/utils/setLogout.ts
+++ src/utils/setLogout.ts
@@ -1,8 +1,12 @@
+import { getStoredData } from './getStoredData'
 import { setItemInStorage } from './setItemInStorage'
 
 export const setLogout = () => {
+    const parsedData = getStoredData()
+    const userName = parsedData?.username || ''
     localStorage.clear()
     setItemInStorage('issigningup', false)
+    setItemInStorage('username', userName)
     window.location.href = '/'
 
     return false
