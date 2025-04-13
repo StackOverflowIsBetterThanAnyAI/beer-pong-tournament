71a007e1b1b8941958d8400ee3fb3c68bf53b29b
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-04-13 20:09:32 +0200

    header

--- src/components/teams/Teams.tsx
+++ src/components/teams/Teams.tsx
@@ -1,5 +1,14 @@
 export const Teams = () => {
-    return <div>Hello Teams!</div>
+    return (
+        <main className="w-full bg-stone-300 text-stone-950 sm:w-80 md:w-112 sm:rounded-lg p-3 sm:p-4 md:p-6">
+            <h1 className="text-center font-semibold text-extra-large">
+                Team Overview
+            </h1>
+            <h2 className="text-center text-large px-1">
+                Currently registered Teams:
+            </h2>
+        </main>
+    )
 }
 
 export default Teams
