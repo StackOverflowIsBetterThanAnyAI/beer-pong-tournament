e87481b3b4adea0d88236050a7cc62520d4357dc
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-04-13 17:46:38 +0200

    typo

--- src/utils/getValueFromError.ts
+++ src/utils/getValueFromError.ts
@@ -1,8 +1,8 @@
-type getValueFromErrorType = {
+type getValueFromErrorProps = {
     [key: string]: string | string[]
 }
 
-export const getValueFromError = (obj: getValueFromErrorType) => {
+export const getValueFromError = (obj: getValueFromErrorProps) => {
     const value = Object.values(obj)[0]
     typeof value === 'string' ? value : value.join('') || ''
     return value.toString()
