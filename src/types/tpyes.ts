31be199299f3255f673c1a9f472309ad6d83a0c8
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-04-14 20:00:14 +0200

    outsource more frequently used types

---
+++ src/types/tpyes.ts
@@ -0,0 +1,10 @@
+export type LoggedInUserProps = {
+    user: string
+    token: string
+}
+
+export type RegisteredTeamsProps = {
+    name: string
+    member_one: string
+    member_two: string
+}[]
