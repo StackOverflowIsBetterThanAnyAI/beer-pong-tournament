8fabf114765b7f4e7d8bab6cfb1ee3ad1bcd3220
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-04-15 20:40:10 +0200

    registeredteams and registeredteam props

--- src/types/tpyes.ts
+++ src/types/tpyes.ts
@@ -3,8 +3,11 @@ export type LoggedInUserProps = {
     token: string
 }
 
-export type RegisteredTeamsProps = {
+export type RegisteredTeamProps = {
+    id?: string
     name: string
     member_one: string
     member_two: string
-}[]
+}
+
+export type RegisteredTeamsProps = RegisteredTeamProps[]
