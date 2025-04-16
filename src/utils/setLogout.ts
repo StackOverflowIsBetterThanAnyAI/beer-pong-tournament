dc1ed6f05e369aa5430f2c94129657aeef0712cb
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-04-16 17:17:20 +0200

    set window location href to '/' before clearing the storage to fix brief page not found screen

--- src/utils/setLogout.ts
+++ src/utils/setLogout.ts
@@ -4,10 +4,10 @@ import { setItemInStorage } from './setItemInStorage'
 export const setLogout = () => {
     const parsedData = getStoredData()
     const userName = parsedData?.username || ''
+    window.location.href = '/'
     localStorage.clear()
     setItemInStorage('issigningup', false)
     setItemInStorage('username', userName)
-    window.location.href = '/'
 
     return false
 }
