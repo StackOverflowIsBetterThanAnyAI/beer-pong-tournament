201dee017a14d6fb0bb6886898d59feadfb4a2f6
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-04-16 23:16:08 +0200

    use constants for teams per group and the minimum amount of teams for a tournament

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -4,6 +4,10 @@ export const MAX_ITEMS_DESKTOP = 8
 
 export const MAX_TEAMS = 32
 
+export const MIN_TEAMS = 8
+
+export const TEAMS_PER_GROUP = 4
+
 export const ROUTES = ['home', 'groups', 'teams', 'register']
 
 export const SERVER_ADDRESS = 'http://localhost:8000'
