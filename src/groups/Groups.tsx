a1bd9db1e58be1e4e6d3631a7b7f25be99163456
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-04-22 22:13:50 +0200

    filter teams based on page size

--- src/groups/Groups.tsx
+++ src/groups/Groups.tsx
@@ -25,48 +25,56 @@ const Groups = ({ groups, page, setPage }: GroupsProps) => {
     return (
         <>
             <ul className="flex flex-col gap-2 w-full max-w-80 bg-stone-400 drop-shadow-stone-600/60 drop-shadow-md my-4 p-2 m-auto rounded-sm">
-                {groups.map((item, index) => {
-                    return (
-                        <li
-                            key={item.id}
-                            className={`p-2 rounded-sm ${
-                                index % 2 ? 'bg-red-100' : 'bg-stone-200'
-                            }`}
-                        >
-                            <h2 className="text-large font-bold underline">
-                                {item.name}
-                            </h2>
-                            <ul className="flex flex-col gap-2 pt-1">
-                                {item.teams.map((i) => {
-                                    return (
-                                        <li
-                                            key={i.id}
-                                            className={`text-normal font-normal border-b-2 ${
-                                                index % 2
-                                                    ? 'border-red-500'
-                                                    : 'border-stone-600'
-                                            } `}
-                                        >
-                                            <div className="flex flex-col">
-                                                <div className="font-bold text-ellipsis overflow-hidden">
-                                                    {i.name}
-                                                </div>
-                                                <div className="flex justify-between">
-                                                    <div className="text-ellipsis overflow-hidden">
-                                                        {i.member_one}
+                {groups
+                    .filter((item, index) => {
+                        if (
+                            index >= (page - 1) * MAX_ITEMS_PER_PAGE &&
+                            index < page * MAX_ITEMS_PER_PAGE
+                        )
+                            return item
+                    })
+                    .map((i, x) => {
+                        return (
+                            <li
+                                key={i.id}
+                                className={`p-2 rounded-sm ${
+                                    x % 2 ? 'bg-red-100' : 'bg-stone-200'
+                                }`}
+                            >
+                                <h2 className="text-large font-bold underline">
+                                    {i.name}
+                                </h2>
+                                <ul className="flex flex-col gap-2 pt-1">
+                                    {i.teams.map((t) => {
+                                        return (
+                                            <li
+                                                key={t.id}
+                                                className={`text-normal font-normal border-b-2 ${
+                                                    x % 2
+                                                        ? 'border-red-500'
+                                                        : 'border-stone-600'
+                                                } `}
+                                            >
+                                                <div className="flex flex-col">
+                                                    <div className="font-bold text-ellipsis overflow-hidden">
+                                                        {t.name}
                                                     </div>
-                                                    <div className="text-ellipsis overflow-hidden">
-                                                        {i.member_two}
+                                                    <div className="flex justify-between">
+                                                        <div className="text-ellipsis overflow-hidden">
+                                                            {t.member_one}
+                                                        </div>
+                                                        <div className="text-ellipsis overflow-hidden">
+                                                            {t.member_two}
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        </li>
-                                    )
-                                })}
-                            </ul>
-                        </li>
-                    )
-                })}
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                            </li>
+                        )
+                    })}
             </ul>
             <PageNavigation
                 MAX_ITEMS_PER_PAGE={MAX_ITEMS_PER_PAGE}
