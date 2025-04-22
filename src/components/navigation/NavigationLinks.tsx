4f5640e110cf3395ec1565f283617852168d98f9
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-04-22 19:52:17 +0200

    store filtered routes in separate variable

--- src/components/navigation/NavigationLinks.tsx
+++ src/components/navigation/NavigationLinks.tsx
@@ -10,28 +10,26 @@ const NavigationLinks = () => {
         setIsNavigationExpanded((prev) => !prev)
     }
 
+    const routes = ROUTES.filter((item: string) => item !== 'home').map((i) => {
+        return (
+            <a
+                key={i}
+                href={getValidHref(i)}
+                className="underline rounded-md px-2 py-0.5 focus-visible:bg-stone-100/50 hover:bg-red-300/50 active:bg-red-300"
+            >
+                {i
+                    .split(' ')
+                    .map(
+                        (x) => x.substring(0, 1).toUpperCase() + x.substring(1)
+                    )
+                    .join(' ')}
+            </a>
+        )
+    })
+
     return (
         <nav className="flex flex-col gap-1 bg-red-200 text-stone-950 w-full p-4">
-            {isNavigationExpanded
-                ? ROUTES.filter((item: string) => item !== 'home').map((i) => {
-                      return (
-                          <a
-                              key={i}
-                              href={getValidHref(i)}
-                              className="underline rounded-md px-2 py-0.5 focus-visible:bg-stone-100/50 hover:bg-red-300/50 active:bg-red-300"
-                          >
-                              {i
-                                  .split(' ')
-                                  .map(
-                                      (x) =>
-                                          x.substring(0, 1).toUpperCase() +
-                                          x.substring(1)
-                                  )
-                                  .join(' ')}
-                          </a>
-                      )
-                  })
-                : null}
+            {isNavigationExpanded ? routes : null}
             <button
                 onClick={handleClick}
                 className="bg-stone-100/90 outline outline-red-400 rounded-md mt-2 focus-visible:bg-stone-50 hover:bg-red-100 active:bg-red-200"
