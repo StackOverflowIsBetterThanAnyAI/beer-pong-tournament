755b73385ad845807de885adb0f2c30fc74b9a8f
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-04-23 17:02:32 +0200

    return group / team names instead of their ids

--- src/types/types.ts
+++ src/types/types.ts
@@ -14,9 +14,9 @@ export type RegisteredTeamsProps = RegisteredTeamProps[]
 
 export type ScheduleProps = {
     id: number
-    group: number
-    team1: number
-    team2: number
+    group: string
+    team1: string
+    team2: string
     score_team1: number | null
     score_team2: number | null
     played: boolean
