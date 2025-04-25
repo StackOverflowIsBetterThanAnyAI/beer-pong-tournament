b2381058d3a013a5f5a1bf8dffbbe9b4197bbcd7
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-04-25 16:04:58 +0200

    outsource schedule items

--- src/components/schedule/ScheduleItem.tsx
+++ src/components/schedule/ScheduleItem.tsx
@@ -1,13 +1,14 @@
+import { useContext } from 'react'
 import PageNavigation from '../page/PageNavigation'
+import ScheduleItemScore from './ScheduleItemScore'
 import { MATCHES_PER_GROUP } from '../../constants/constants'
-import { ScheduleProps } from '../../types/types'
+import { ContextSchedule } from '../../context/ContextSchedule'
 
 type ScheduleItemProps = {
     MAX_ITEMS_PER_PAGE: number
     nextPage: () => void
     page: number
     previousPage: () => void
-    schedule: ScheduleProps
 }
 
 const ScheduleItem = ({
@@ -15,8 +16,15 @@ const ScheduleItem = ({
     nextPage,
     page,
     previousPage,
-    schedule,
 }: ScheduleItemProps) => {
+    const contextSchedule = useContext(ContextSchedule)
+    if (!contextSchedule) {
+        throw new Error(
+            'ScheduleItem must be used within a ContextSchedule.Provider'
+        )
+    }
+    const [schedule, _setSchedule] = contextSchedule
+
     return (
         <>
             <ul className="flex flex-col gap-2 w-full max-w-80 bg-stone-400 drop-shadow-stone-600/60 drop-shadow-md my-4 p-2 m-auto rounded-sm">
@@ -48,62 +56,7 @@ const ScheduleItem = ({
                                 <ul className="flex flex-col gap-2 pt-1">
                                     <li className="text-normal font-normal">
                                         <div className="flex flex-col">
-                                            <div className="flex flex-wrap max-[260px]:flex-col justify-between gap-x-2 font-bold text-ellipsis overflow-hidden">
-                                                <label
-                                                    htmlFor={`input${i.team1}vs${i.team2}`}
-                                                >
-                                                    {i.team1}
-                                                </label>
-                                                <span className="m-1">
-                                                    {i.score_team1 ?? (
-                                                        <input
-                                                            id={`input${i.team1}vs${i.team2}`}
-                                                            type="number"
-                                                            min={0}
-                                                            max={10}
-                                                            className={`${
-                                                                x % 2
-                                                                    ? 'bg-red-400 !outline-red-600'
-                                                                    : 'bg-stone-400/70 !outline-stone-500'
-                                                            } w-full pl-1 rounded-sm`}
-                                                        />
-                                                    )}
-                                                </span>
-                                            </div>
-                                            <div className="flex flex-wrap max-[260px]:flex-col justify-between gap-x-2 font-bold text-ellipsis overflow-hidden">
-                                                <label
-                                                    htmlFor={`input${i.team2}vs${i.team1}`}
-                                                >
-                                                    {i.team2}
-                                                </label>
-                                                <span className="m-1">
-                                                    {i.score_team2 ?? (
-                                                        <input
-                                                            id={`input${i.team2}vs${i.team1}`}
-                                                            type="number"
-                                                            min={0}
-                                                            max={10}
-                                                            className={`${
-                                                                x % 2
-                                                                    ? 'bg-red-400 !outline-red-600'
-                                                                    : 'bg-stone-400/70 !outline-stone-500'
-                                                            } w-full pl-1 rounded-sm`}
-                                                        />
-                                                    )}
-                                                </span>
-                                            </div>
-                                            <button
-                                                className={`bg-stone-100/90 outline text-normal rounded-md mt-2 p-0.5 focus-visible:bg-stone-50 
-                                                    ${
-                                                        x % 2
-                                                            ? 'outline-red-400 hover:bg-red-100 active:bg-red-200'
-                                                            : 'outline-stone-500 hover:bg-stone-200/80 active:bg-stone-300'
-                                                    }`}
-                                                aria-label="Save Score"
-                                                title="Save Score"
-                                            >
-                                                Save Score
-                                            </button>
+                                            <ScheduleItemScore i={i} x={x} />
                                         </div>
                                     </li>
                                 </ul>
