2aba6cb41dffb5581f3168bda67e86eeae92c159
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-04-25 16:05:20 +0200

    submit button to trigger patch request

---
+++ src/components/schedule/ScheduleItemButton.tsx
@@ -0,0 +1,60 @@
+import { FetchLoading } from 'fetch-loading'
+import { GameProps } from '../../types/types'
+
+type ScheduleItemButtonProps = {
+    handleClick: (id: number, score_team1: string, score_team2: string) => void
+    i: GameProps
+    isLoading: boolean
+    scoreTeam1: string | null
+    scoreTeam2: string | null
+    x: number
+}
+
+const ScheduleItemButton = ({
+    handleClick,
+    i,
+    isLoading,
+    scoreTeam1,
+    scoreTeam2,
+    x,
+}: ScheduleItemButtonProps) => {
+    return (
+        <button
+            className={`flex justify-center bg-stone-100/90 outline text-normal rounded-md mt-2 p-0.5 focus-visible:bg-stone-50 
+            ${
+                x % 2
+                    ? 'outline-red-400 not-[:disabled]:hover:bg-red-100 active:bg-red-200 disabled:bg-red-50/20'
+                    : 'outline-stone-500 not-[:disabled]:hover:bg-stone-200/80 active:bg-stone-300 disabled:bg-stone-50/20'
+            }`}
+            aria-label={
+                i.played ? 'Match Over. Score has been saved.' : 'Save Score.'
+            }
+            title={
+                i.played ? 'Match Over. Score has been saved.' : 'Save Score.'
+            }
+            onClick={() => handleClick(i.id, scoreTeam1!, scoreTeam2!)}
+            aria-disabled={
+                i.played ||
+                !scoreTeam1 ||
+                !scoreTeam2 ||
+                (scoreTeam1 !== '10' && scoreTeam2 !== '10')
+            }
+            disabled={
+                i.played ||
+                !scoreTeam1 ||
+                !scoreTeam2 ||
+                (scoreTeam1 < '10' && scoreTeam2 < '10')
+            }
+        >
+            {isLoading ? (
+                <FetchLoading theme="#44403c" />
+            ) : i.played ? (
+                'Match Over'
+            ) : (
+                'Save Score'
+            )}
+        </button>
+    )
+}
+
+export default ScheduleItemButton
