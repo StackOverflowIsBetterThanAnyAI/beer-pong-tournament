fa8db6ef366612257a50df79fd8f579a7137ca5c
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-04-27 13:17:27 +0200

    fetch standings

---
+++ src/api/handleLoadStandings.ts
@@ -0,0 +1,59 @@
+import { SERVER_ADDRESS } from '../constants/constants'
+import { StandingsProps } from '../types/types'
+import { getValidToken } from '../utils/getValidToken'
+import { getValueFromError } from '../utils/getValueFromError'
+import { setItemInStorage } from '../utils/setItemInStorage'
+
+type handleLoadStandingsProps = {
+    accessToken: string
+    refreshToken: string
+    setApiError: (value: React.SetStateAction<string>) => void
+    setIsLoading: React.Dispatch<React.SetStateAction<boolean>>
+    setStandings: React.Dispatch<React.SetStateAction<StandingsProps>>
+}
+
+export const handleLoadStandings = async ({
+    accessToken,
+    refreshToken,
+    setApiError,
+    setIsLoading,
+    setStandings,
+}: handleLoadStandingsProps) => {
+    setIsLoading(true)
+    setApiError('')
+    setItemInStorage('standings', [])
+    setStandings([])
+
+    try {
+        const response = await fetch(
+            `${SERVER_ADDRESS}/api/v1/groups/standings/`,
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${await getValidToken(
+                        accessToken,
+                        refreshToken
+                    )}`,
+                },
+            }
+        )
+
+        if (!response.ok) {
+            const errorData = await response.json()
+            setApiError(
+                getValueFromError(errorData) ||
+                    'An error occurred while fetching the current standings.'
+            )
+            return
+        }
+
+        const standings = await response.json()
+        setItemInStorage('standings', standings)
+        setStandings(standings)
+    } catch (error: any) {
+        setApiError('An error occurred while fetching the current standings.')
+    } finally {
+        setIsLoading(false)
+    }
+}
