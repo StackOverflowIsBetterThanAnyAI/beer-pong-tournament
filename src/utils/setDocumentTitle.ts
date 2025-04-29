187f9edfec8a7d9721122d522c7d8be3e3bdd8b2
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-04-29 19:28:33 +0200

    outsource getValidRoute

--- src/utils/setDocumentTitle.ts
+++ src/utils/setDocumentTitle.ts
@@ -1,22 +1,10 @@
-import { ROUTES } from './../constants/constants'
 import { getValidPathname } from './getValidPathname'
+import { getValidRoute } from './getValidRoute'
 
 export const setDocumentTitle = () => {
     const pathname = getValidPathname().length ? getValidPathname() : ['home']
 
-    return `Beer Pong Tournament | ${
-        ROUTES.includes(pathname.join('/'))
-            ? pathname.map((item) => {
-                  return item
-                      .split('-')
-                      .map((i) => {
-                          return (
-                              i.substring(0, 1).toUpperCase() +
-                              i.substring(1).toLowerCase()
-                          )
-                      })
-                      .join(' ')
-              })
-            : '404 Not Found'
-    }`
+    const validRoute = getValidRoute(pathname)
+
+    return `Beer Pong Tournament | ${validRoute || '404 Not Found'}`
 }
