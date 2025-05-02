fa3274f9f1a3e23e11c5f06b5d4cc5f313648efb
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-05-02 09:24:27 +0200

    new subheader for unfinished group stage, add text-pretty to subheaders

--- src/components/form/FormHeader.tsx
+++ src/components/form/FormHeader.tsx
@@ -6,27 +6,33 @@ type FormHeaderProps = {
 const FormHeader = ({ header, subHeader }: FormHeaderProps) => {
     const subHeaderElement = (() => {
         switch (subHeader) {
+            case 'knockout':
+                return (
+                    <h2 className="text-center text-pretty text-large px-1">
+                        Not all Group Stage Matches have been played yet.
+                    </h2>
+                )
             case 'no content':
                 return (
-                    <h2 className="text-center text-large px-1">
+                    <h2 className="text-center text-pretty text-large px-1">
                         The Tournament has not started yet.
                     </h2>
                 )
             case 'no content ko':
                 return (
-                    <h2 className="text-center text-large px-1">
+                    <h2 className="text-center text-pretty text-large px-1">
                         The Knockout Stage has not started yet.
                     </h2>
                 )
             case 'schedule':
                 return (
-                    <h2 className="text-center text-large px-1">
+                    <h2 className="text-center text-pretty text-large px-1">
                         Pairings and Scores:
                     </h2>
                 )
             case 'required':
                 return (
-                    <h2 className="text-center text-large px-1">
+                    <h2 className="text-center text-pretty text-large px-1">
                         All fields marked with{' '}
                         <span className="text-red-800">*</span> are required.
                     </h2>
