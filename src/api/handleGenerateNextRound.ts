4d573984ebf67c286c29a9ab301bb5d5a5807ba1
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-05-02 17:14:15 +0200

    generate next round of knockout stage

---
+++ src/api/handleGenerateNextRound.ts
@@ -0,0 +1,64 @@
+import { SERVER_ADDRESS } from '../constants/constants'
+import { KOStageProps } from '../types/types'
+import { getValidToken } from '../utils/getValidToken'
+import { getValueFromError } from '../utils/getValueFromError'
+import { handleLoadKOStage } from './handleLoadKOStage'
+
+type handleGenerateNextRoundProps = {
+    accessToken: string
+    currentRound: string
+    nextRound: string
+    refreshToken: string
+    setApiError: (value: React.SetStateAction<string>) => void
+    setKOStage: React.Dispatch<React.SetStateAction<KOStageProps>>
+}
+
+export const handleGenerateNextRound = async ({
+    accessToken,
+    currentRound,
+    nextRound,
+    refreshToken,
+    setApiError,
+    setKOStage,
+}: handleGenerateNextRoundProps) => {
+    const roundData = {
+        current_round: currentRound,
+        next_round: nextRound,
+    }
+    try {
+        const response = await fetch(
+            `${SERVER_ADDRESS}/api/v1/ko-stage/next-round/`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${await getValidToken(
+                        accessToken,
+                        refreshToken
+                    )}`,
+                },
+                body: JSON.stringify(roundData),
+            }
+        )
+
+        if (!response.ok) {
+            const errorData = await response.json()
+            setApiError(
+                getValueFromError(errorData) ||
+                    'An error occurred while generating the next round of the Knockout Stage.'
+            )
+            return
+        }
+
+        handleLoadKOStage({
+            accessToken,
+            refreshToken,
+            setApiError,
+            setKOStage,
+        })
+    } catch (error: any) {
+        setApiError(
+            'An error occurred while generating the next round of the Knockout Stage.'
+        )
+    }
+}
