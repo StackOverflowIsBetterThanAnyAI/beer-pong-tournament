a9e6edef5a47f1e402b61c4664acecd1df172f08
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-05-04 19:57:21 +0200

    new icon "open in new tab", set github repository

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -20,11 +20,24 @@ const Footer = () => {
                 Reserved.
             </span>
             <a
-                href="/"
+                href="https://github.com/StackOverflowIsBetterThanAnyAI/beer-pong-tournament"
                 onKeyDown={(e) => handleKeyDown(e)}
-                className="w-fit px-4 py-1 rounded-sm m-auto underline hover:bg-stone-700 active:bg-stone-600"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="GitHub Repository (opens in new tab)"
+                className="w-fit flex items-center gap-1 px-4 py-1 rounded-sm m-auto underline hover:bg-stone-700 active:bg-stone-600"
             >
-                GitHub Repository
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    height={12}
+                    fill="#f4f4f5"
+                    aria-hidden="true"
+                    focusable="false"
+                >
+                    <path d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z" />
+                </svg>
+                <span>GitHub Repository</span>
             </a>
         </footer>
     )
