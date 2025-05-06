3099ee66a05067ae52643fd62f45f30d7dcf1fe4
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-05-06 18:51:11 +0200

    bullet points, add more breathing room between items

--- src/components/standings/StandingsLegend.tsx
+++ src/components/standings/StandingsLegend.tsx
@@ -26,30 +26,36 @@ const StandingsLegend = ({ standings }: StandingsLegendProps) => {
     return (
         <div className="flex flex-col w-full max-w-96 bg-stone-200 p-2 rounded-sm m-auto">
             <span className="flex justify-between z-10">
-                <h3 className="text-normal">Legend:</h3>
+                <h3 className="text-normal mb-1">Legend:</h3>
                 <span className="legendinfo relative bg-stone-200 text-center text-normal h-5 w-5 text-red-600 outline-2 outline-red-600 rounded-full">
                     !
                 </span>
             </span>
-            <ul className="text-small">
-                <li>The first two teams of each group advance.</li>
+            <ul className="text-small list-disc">
+                <li className="ml-4 my-0.5">
+                    The first two teams of each group advance.
+                </li>
                 {thirdAdvance ? (
                     <>
-                        <li>
+                        <li className="ml-4 my-0.5">
                             Out of all groups, the {thirdAdvance} best
                             third-placed teams advance.
                         </li>
-                        <li>
+                        <li className="ml-4 my-0.5">
                             The other{' '}
                             {thirdEliminated === 1 ? '' : thirdEliminated}{' '}
                             third-placed team
                             {thirdEliminated === 1 ? '' : 's'}{' '}
                             {thirdEliminated === 1 ? 'is' : 'are'} eliminated.
                         </li>
-                        <li>All last-placed teams are eliminated.</li>
+                        <li className="ml-4 my-0.5">
+                            All last-placed teams are eliminated.
+                        </li>
                     </>
                 ) : (
-                    <li>The last two teams of each group are eliminated.</li>
+                    <li className="ml-4 my-0.5">
+                        The last two teams of each group are eliminated.
+                    </li>
                 )}
             </ul>
         </div>
