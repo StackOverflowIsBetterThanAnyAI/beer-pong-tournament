d2cfb714a3ddfb1eda65f89a0598975432ef73bf
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-05-06 18:27:32 +0200

    update dependencies if ref?.current changes

--- src/hooks/useAutoFocus.ts
+++ src/hooks/useAutoFocus.ts
@@ -3,5 +3,5 @@ import { useEffect } from 'react'
 export const useAutoFocus = (ref: React.RefObject<any>) => {
     useEffect(() => {
         ref?.current?.focus()
-    }, [ref])
+    }, [ref?.current])
 }
