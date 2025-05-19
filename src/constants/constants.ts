948f3e130963c71e7d80881847153c89487eac96
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-05-19 18:30:02 +0200

    prod / test environment

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -31,4 +31,4 @@ export const ROUTES = [
 ]
 
 export const SERVER_ADDRESS =
-    'https://beer-pong-tournament-backend.onrender.com'
+    import.meta.env.VITE_SERVER_ADDRESS || 'http://localhost:8000'
