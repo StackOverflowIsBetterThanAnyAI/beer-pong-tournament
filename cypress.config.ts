7eb8621a34c6338bb8a9ebf70b8069fb39fce586
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-05-21 19:13:23 +0200

    env variables

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'cypress'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export default defineConfig({
+    env: {
+        ADMIN_USERNAME: process.env.CYPRESS_ADMIN_USERNAME,
+        ADMIN_PASSWORD: process.env.CYPRESS_ADMIN_PASSWORD,
+    },
     e2e: {
         baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:5173',
     },
