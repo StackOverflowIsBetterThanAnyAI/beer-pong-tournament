0bceb1d5b4f9d71720796cdb11f8cb1f3121d042
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-05-22 16:20:41 +0200

    prdo / local config

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,8 +5,12 @@ dotenv.config()
 
 export default defineConfig({
     env: {
-        ADMIN_USERNAME: process.env.CYPRESS_ADMIN_USERNAME,
-        ADMIN_PASSWORD: process.env.CYPRESS_ADMIN_PASSWORD,
+        ADMIN_USERNAME:
+            process.env.CYPRESS_ADMIN_USERNAME ||
+            process.env.CYPRESS_ADMIN_USERNAME_LOCAL,
+        ADMIN_PASSWORD:
+            process.env.CYPRESS_ADMIN_PASSWORD ||
+            process.env.CYPRESS_ADMIN_PASSWORD_LOCAL,
     },
     e2e: {
         baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:5173',
