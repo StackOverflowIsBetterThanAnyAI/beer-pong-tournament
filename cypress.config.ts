20bdd1cf913ac854231f2dd2de729163165a1183
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-05-22 16:24:47 +0200

    solve conflict with local and prod secrets

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,12 +5,10 @@ dotenv.config()
 
 export default defineConfig({
     env: {
-        ADMIN_USERNAME:
-            process.env.CYPRESS_ADMIN_USERNAME ||
-            process.env.CYPRESS_ADMIN_USERNAME_LOCAL,
-        ADMIN_PASSWORD:
-            process.env.CYPRESS_ADMIN_PASSWORD ||
-            process.env.CYPRESS_ADMIN_PASSWORD_LOCAL,
+        ADMIN_USERNAME: process.env.CYPRESS_ADMIN_USERNAME,
+        ADMIN_USERNAME_LOCAL: process.env.CYPRESS_ADMIN_USERNAME_LOCAL,
+        ADMIN_PASSWORD: process.env.CYPRESS_ADMIN_PASSWORD,
+        ADMIN_PASSWORD_LOCAL: process.env.CYPRESS_ADMIN_PASSWORD_LOCAL,
     },
     e2e: {
         baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:5173',
