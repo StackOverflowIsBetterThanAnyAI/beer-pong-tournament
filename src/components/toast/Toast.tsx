25b6706bdf6dd4f0b83da5105ac92d596d709229
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2025-08-27 16:56:48 +0200

    add button to close toast notification

--- src/components/toast/Toast.tsx
+++ src/components/toast/Toast.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
+import { ContextIsToastVisible } from '../../context/ContextIsToastVisible'
 
 type SuccessProps = {
     isSuccess: boolean
@@ -6,6 +7,14 @@ type SuccessProps = {
 }
 
 const Toast = ({ isSuccess, label }: SuccessProps) => {
+    const contextIsToastVisible = useContext(ContextIsToastVisible)
+    if (!contextIsToastVisible) {
+        throw new Error(
+            'FormInputPassword must be used within a ContextIsToastVisible.Provider'
+        )
+    }
+    const [_isVisible, setIsVisible] = contextIsToastVisible
+
     const [isTriggered, setIsTriggered] = useState<boolean>(true)
 
     const opacity = isTriggered ? 'opacity-90' : 'opacity-0'
@@ -17,14 +26,27 @@ const Toast = ({ isSuccess, label }: SuccessProps) => {
         setTimeout(() => setIsTriggered(false), 3000)
     }, [])
 
+    const handleCloseToast = () => {
+        setIsTriggered(false)
+        setIsVisible(false)
+    }
+
     return (
         <div
-            className={`text-normal fixed bottom-6 left-6 rounded-sm
+            className={`text-normal fixed flex gap-2 bottom-6 left-6 rounded-sm
             outline-1 outline-zinc-50 p-2 transition-[opacity] duration-700 ${opacity} ${theme}`}
             role="status"
             aria-live="polite"
         >
             {label}
+            <button
+                className="flex items-center justify-center rounded-full outline-1 outline-stone-900 text-sm h-4 w-4 bg-zinc-50/80"
+                onClick={handleCloseToast}
+                aria-label="Close Notification"
+                title="Close Notification"
+            >
+                ×
+            </button>
         </div>
     )
 }
