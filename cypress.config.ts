4f9a962652227037bad91e1b82d64aaf25ea32ee
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2026-06-17 20:07:23 +0200

    delete unnecessary files

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,27 +1,11 @@
 import { defineConfig } from 'cypress'
-import dotenv from 'dotenv'
-
-dotenv.config()
 
 export default defineConfig({
-    env: {
-        ADMIN_USERNAME: process.env.CYPRESS_ADMIN_USERNAME,
-        ADMIN_USERNAME_LOCAL: process.env.CYPRESS_ADMIN_USERNAME_LOCAL,
-        ADMIN_PASSWORD: process.env.CYPRESS_ADMIN_PASSWORD,
-        ADMIN_PASSWORD_LOCAL: process.env.CYPRESS_ADMIN_PASSWORD_LOCAL,
-    },
     e2e: {
-        setupNodeEvents(on, config) {
-            on('task', {
-                log(message) {
-                    console.log(message)
-                    return null
-                },
-            })
-            config.env = process.env
+        setupNodeEvents(_on, config) {
             return config
         },
-        baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:5173',
+        baseUrl: 'http://localhost:5173',
         defaultCommandTimeout: 90000,
     },
 })
